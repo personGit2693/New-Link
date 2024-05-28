d66697d9f85a0e36f9ccbe98acc9628afe447a9e
Marcel Hector Gueco <marcel.gueco@gmail.com>
2024-05-28 09:34:40 -0400

    Added src folder for images and set HTML contents

--- index.php
+++ index.php
@@ -6,8 +6,10 @@
 	<title>New Website Link</title>
 </head>
 <body>
-	Hello World New Link
-	Rogie Bogie(Ako Hector)
-	Test Mike
+	<div>
+		<div><img src="Module Index/src/importantnotice.png"></div>
+		<div>We already moved to another web address! To get to our new page</div>
+		<div><a href=""><img src="Module Index/src/clickherebutton.png"></a></div>
+	</div>
 </body>
 </html>
