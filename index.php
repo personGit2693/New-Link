1cbe4e86d14ed0d93a3b120e1b013d2a1742de52
Marcel Hector Gueco <marcel.gueco@gmail.com>
2024-05-29 07:50:23 -0400

    Added HTML and CSS to index page

--- index.php
+++ index.php
@@ -3,13 +3,16 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="Module Index/Styles/index.css">
 	<title>New Website Link</title>
 </head>
 <body>
-	<div>
-		<div><img src="Module Index/src/importantnotice.png"></div>
-		<div>We already moved to another web address! To get to our new page</div>
-		<div><a href=""><img src="Module Index/src/clickherebutton.png"></a></div>
+	<div class="firstDivWrap_class">
+		<div class="contentDivWrap_class">
+			<div class="imgWrap1_class"><img src="Module Index/src/importantnotice.png"></div>
+			<div class="msgText_class">We already moved to another web address! Here's a link to our new page:</div>
+			<div class="imgWrap2_class"><a class="clickButton_class" href="">www.VeteransMedicalCenter.com<!--img class="clickButton_class"src="Module Index/src/clickherebutton.png"--></a></div-->
+		</div>
 	</div>
 </body>
 </html>
